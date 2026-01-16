6bb56e3d2df45de30e2747cb7fca2d5b917ab451
shaikhNoorAbbas <shaikhnoor1512@gmail.com>
2026-01-16 20:45:02 +0530

    added starter boilerplate

--- backend/routes/bookRoutes.js
+++ backend/routes/bookRoutes.js
@@ -1,1 +1,13 @@
-console.log("Book Routes");
+import express from "express";
+
+let router = express.Router();
+
+// desc:-> get all books Data
+// endpoint is: http:localhost:1000/books/
+router.get("/", (req, res) => {
+  res.json({
+    message: "Get All Books Data",
+  });
+});
+
+export default router;
--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,26 @@
-console.log("Server side code")
+import express from "express";
+import cors from "cors";
+import bookRoutes from "./routes/bookRoutes.js";
+//Assinging Express to an App
+let app = express();
+
+//Middleware-> JSON Parser to Get JSON Data
+app.use(express.json());
+
+// middleware to allow frontend to communicate with this backend
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+
+//middleware to use routes
+app.use("/books", bookRoutes);
+
+let port = 1000;
+//Starting a Server
+app.listen(port, () =>
+  console.log(
+    `Port is Running on: ${port} \n Location is: http://localhost:${port}`
+  )
+);
